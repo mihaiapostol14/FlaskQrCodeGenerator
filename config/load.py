6730db9ab6753f4ca91854efcaacd74a25b8db84
Mihai Apostol <mihaiapostol153@gmail.com>
2026-09-21 11:24:13 +0300

    added config

---
+++ config/load.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+# Code of your application, which uses environment variables (e.g. from `os.environ` or
+# `os.getenv`) as if they came from the actual environment.
+
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
